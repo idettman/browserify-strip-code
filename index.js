494a928a98fe197aa7ffd2761ca8265aa3cfab3b
Isaac Dettman <idettman@rubiconproject.com>
2019-02-22 06:19:22 -0800

    add file whitelist option to only process code replace in files included in whitelist array

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const through2 = require('through2');
 
 module.exports = function (file, opts) {
   let code = '';
+    
   return through2.obj(function stripCodeThrough(/**Buffer*/buf, enc, next) {
     /*  accumulate the code chunks  */
     code += buf.toString('utf8')
     next()
   }, function stripCodeThroughNext(next) {
-    /*  transform the code  */
-    code = code.replace(/\/\* start-test-block \*\/((?:.|\n)*)\/\* end-test-block \*\//gm, '')
+    if (!opts || !Array.isArray(opts.whitelist) || opts.whitelist.indexOf(file) !== -1) {
+      /*  transform the code  */
+      code = code.replace(/\/\* start-test-block \*\/((?:.|\n)*)\/\* end-test-block \*\//gm, '')
+    }
     this.push(Buffer.from(code))
     next()
   })
